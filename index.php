2a180e6958205870b75c5829901e26481290199f
Superwow3D <superwow@bk.ru>
2019-11-14 16:26:41 +0100

    Added PHPMailer

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: amijami
- * Date: 2019-11-14
- * Time: 13:12
- */
+
+use App\Participant\Participant;
+use App\SecretSanta\SecretSanta;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+include_once 'vendor/autoload.php';
+
+try {
+    $santa = (new SecretSanta())
+        ->addParticipant(new Participant('Amir', 'some@mail'))
+        ->addParticipant(new Participant('Amir', 'some@mail'))
+        ->addParticipant(new Participant('Amir', 'some@mail'))
+        ->addParticipant(new Participant('Amir', 'some@mail'))
+        ->addParticipant(new Participant('Amir', 'some@mail'))
+        ->addParticipant(new Participant('Amir', 'some@mail'));
+
+    $list = $santa->getShuffledList();
+} catch (Exception $exception) {
+    $exception->getMessage();
+}
+
+$mailer = new PHPMailer(true);
